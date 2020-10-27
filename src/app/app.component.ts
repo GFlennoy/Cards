b474e00ea7ca99180c3dfe9002c09871d2e09ad9
Grant Flennoy <flennoyg@gmail.com>
2020-10-27 17:08:11 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'yeezy',
+      content: 'I saw this neat tree today, I am a genius'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'jeezy',
+      content: 'Weatherman says theres a slight chance of snow'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'segurat',
+      content: 'BIKES?!'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'segurat',
+      content: 'BIKES?!'
+    }
+  ]
 }
